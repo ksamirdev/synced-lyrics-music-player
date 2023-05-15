24edb87808909fa1218adcfb693799de1827605d
Samir <samirdiff@proton.me>
2023-05-15 16:52:12 +0530

    Update Lyrics.ts

--- backend/src/routes/Lyrics.ts
+++ backend/src/routes/Lyrics.ts
@@ -3,7 +3,7 @@ import LyricsClient from "sync-lyrics";
 
 const router = Router();
 const client = new LyricsClient(
-  "AQB4V6oYwtQ7O-Zh2NK7yU_ZUGdcpimzmhvVaKnTNbbZKvSJvNBZPtea-UYH67cY4vMa3JNmy5GR6sADjcJLJzfigJyFUz0tzO-a-r8soOh1ap35BJFW8iuyDWPWuTb7bmF6mqHVmJA80y_CfWM3SMNYMRbTuqg"
+  process.env.SPOTIFY_COOKIE || "AQB4V6oYwtQ7O-Zh2NK7yU_ZUGdcpimzmhvVaKnTNbbZKvSJvNBZPtea-UYH67cY4vMa3JNmy5GR6sADjcJLJzfigJyFUz0tzO-a-r8soOh1ap35BJFW8iuyDWPWuTb7bmF6mqHVmJA80y_CfWM3SMNYMRbTuqg"
 );
 
 router.get("/lyrics/:id", async (req, res) => {
